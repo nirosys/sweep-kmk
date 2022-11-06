2f7bfa34e94cf741ffe6b3436f536f35e47293be
Richard Giliam <nirosys@gmail.com>
2022-11-06 01:01:55 -0800

    Add on-start hotkey to enable debug, and usb drive

--- boot.py
+++ boot.py
@@ -1,12 +1,25 @@
 import storage
+import board
 import supervisor
+import digitalio
+import usb_cdc
+import usb_hid
 
-# Rename mount point..
+# Rename our CircuitPython drive so that we can use EE Hands.
+# This allows us to plug either side of the keyboard in via USB, and KMK will
+# use the drive name to figure out which side is which.
 storage.remount("/", readonly=False)
 m = storage.getmount("/")
-m.label = "SWEEP-R"
+m.label = "SWEEP-L"
 storage.remount("/", readonly=True)
-storage.enable_usb_drive()
 
-supervisor.set_next_stack_limit(4096 + 4096)
+# If during startup we see the left layer key pressed, then we'll enable the USB
+# drive and serial logging. Otherwise we should keep it quiet.
+debug = digitalio.DigitalInOut(board.D9)
+debug.pull = digitalio.Pull.UP
+if debug.value:
+    storage.disable_usb_drive()
+    usb_cdc.disable()
+    usb_hid.enable(boot_device=1)
 
+debug.deinit()
